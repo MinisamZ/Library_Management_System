9bc9c9110cdaa4365572f4c7e5a82e805f8a4a59
Alexandr <minisam2468@gmail.com>
2022-04-18 17:46:15 +0600

    fix user

--- src/main/java/lib/model/User.java
+++ src/main/java/lib/model/User.java
@@ -10,13 +10,13 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name = "table_id", nullable = false)
     private Long table_id;
     @Column(name = "first_name")
     private String first_name;
     @Column(name = "last_name")
     public String last_name;
-    @Column(name="login")
+    @Column(name = "login")
     private String login;
     @Column(name = "password")
     private String password;
